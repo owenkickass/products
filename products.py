3355d1c4078bdf97062e693884ea2993c9f31c34
Owen Chiu <q0972016679@gmail.com>
2019-05-12 22:24:07 +0900

    2 dimensional list

---
+++ products.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+	name = input('請輸入商品名稱')
+	if name == 'q':
+			break
+	price = input('請輸入價格: ') # 終止後就不問價格
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	#products.append(p)
+	products.append([name, price])
+
+print(products)	
